aab8d1502ffcdca4cfe58f31aa3a71dfaa2f14e2
Christoph Stahl <christoph.stahl@tu-dortmund.de>
2023-03-13 18:03:11 +0100

    Added test for inhabit_and_interpret, as well as intersections as queries

---
+++ tests/example_intersection.py
@@ -0,0 +1,38 @@
+from collections.abc import Callable
+from bcls_python import (
+    Type,
+    Constructor,
+    Arrow,
+    Intersection,
+    Var,
+    FiniteCombinatoryLogic,
+    enumerate_terms,
+    Subtypes,
+    inhabit_and_interpret,
+)
+from bcls_python.enumeration import interpret_term
+
+
+def test() -> None:
+    a: Type[str] = Constructor("a")
+    b: Type[str] = Constructor("b")
+    c: Type[str] = Constructor("c")
+    d: Type[str] = Constructor("d")
+
+    X: str = "X"
+    Y: str = "Y"
+    F: Callable[[str], str] = lambda x: f"F({x})"
+
+    repository: dict[str | Callable[[str], str], Type[str]] = dict(
+        {
+            X: Intersection(Intersection(a, b), d),
+            Y: d,
+            F: Intersection(Arrow(a, b), Arrow(d, Intersection(a, c))),
+        }
+    )
+    for real_result in inhabit_and_interpret(repository, [Intersection(a, d), c]):
+        print(real_result)
+
+
+if __name__ == "__main__":
+    test()
