fc45e10fd30f520537881571ebe0936f26f6bb74
Christoph Stahl <christoph.stahl@tu-dortmund.de>
2023-07-13 09:03:20 +0200

    Added test for enumeration/parameterized grammar

---
+++ tests/parameter_grammar.py
@@ -0,0 +1,27 @@
+from cls.enumeration import enumerate_terms
+from cls.grammar import GVar, ParameterizedTreeGrammar, Predicate, RHSRule
+
+
+def main():
+    grammar = ParameterizedTreeGrammar()
+    grammar.add_rule(
+        "X",
+        RHSRule(
+            {"y": "Y"},
+            [],
+            "x",
+            [GVar("y"), GVar("y")],
+        ),
+    )
+    grammar.add_rule("Y", RHSRule({}, [Predicate(lambda _: True, "⊤")], "y1", []))
+    grammar.add_rule("Y", RHSRule({}, [Predicate(lambda _: False, "⊥")], "y2", []))
+    print(grammar.show())
+
+    enumeration = enumerate_terms("X", grammar)
+
+    for t in enumeration:
+        print(t)
+
+
+if __name__ == "__main__":
+    main()
