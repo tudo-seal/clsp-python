49dd84695d2bb9bf23da5eed732a49e2214bb31a
Andrej Dudenhefner <mrhaandi@gmail.com>
2024-07-05 15:26:41 +0200

    updated test for a real scenario

--- tests/test_infer_substitution.py
+++ tests/test_infer_substitution.py
@@ -117,5 +117,18 @@ class TestDSL(unittest.TestCase):
         print(f"{ty} <= {path}")
         print(f"test8: {sub.infer_substitution(ty, path, groups)}")
 
+    def test_for_real(self) -> None:
+        ty = Intersection(Constructor("In", LVar("a")), Constructor("Out", LVar("b")))
+
+        path1 = Constructor("In", Literal(3, "int"))
+        path2 = Constructor("Out", Literal(4, "int"))
+
+        sub = Subtypes({})
+
+        groups = {"a" : "int", "b" : "int"}
+        print(f"{ty} <= {path1}")
+        print(f"{ty} <= {path2}")
+        print(f"test_for_real: {sub.infer_substitution(ty, path1, groups)} and {sub.infer_substitution(ty, path2, groups)}")
+
 if __name__ == "__main__":
     unittest.main()
